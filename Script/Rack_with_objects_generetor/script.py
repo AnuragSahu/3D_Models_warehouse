58a5eefcea546c61b115f7cf38faeeece0cd0414
AnuragSahu <anuragatiit@gmail.com>
2019-11-05 14:01:25 +0530

    Added Support for multiple import

--- Script/Rack_with_objects_generetor/script.py
+++ Script/Rack_with_objects_generetor/script.py
@@ -6,12 +6,15 @@ import random
 all_box_loc = '../SmallPrimitives/BoxModels/'
 rack_loc = '../SmallPrimitives/Racks/modal.dae'
 
-offset_x = -0.3
+offset_x = -0.5
 z_positions = [0.3 , 1.63, 2.98, 4.35]
 y_positions = [-2.3,-1.2 ,0, 1.5]
 
-box_count = {"BoxA":0, "BoxB":0, "BoxD":0, "BoxF":0, "BoxG":0, "BoxH":0}
+box_count = {"BoxA":0, "BoxB":0, "BoxC":0, "BoxD":0, "BoxF":0, "BoxG":0, "BoxH":0, "BoxI":0}
 
+distance_between_racks_x = 1
+distance_between_racks_y = 2.7
+rack_count = 0
 def to_third_number(dig):
     if(dig==0):
         return "";
@@ -47,7 +50,19 @@ def place_racks_and_objects(rack_location,rack_count):
             bpy.data.objects[model].location.y = cols + rack_location[1]
             bpy.data.objects[model].location.z = rows + rack_location[2]
 
-rack_location = [1,1,0]
-place_racks_and_objects(rack_location,0)
-rack_location = [-1,1,0]
-place_racks_and_objects(rack_location,1)
+def place_rack_pair(location,rack_count,distance_between_racks_x):
+    distance_between_racks_x += 1            
+    rack_location = [-distance_between_racks_x+location[0],
+                      distance_between_racks_y+location[1],
+                      0+location[2]]
+    place_racks_and_objects(rack_location,rack_count)
+    rack_location = [ distance_between_racks_x+location[0],
+                      distance_between_racks_y+location[1],
+                      0+location[2]]
+    place_racks_and_objects(rack_location,rack_count+1)
+    return rack_count+2
+
+
+rack_count = place_rack_pair([0,0,0],rack_count,0)
+rack_count = place_rack_pair([0,2*2.7,0],rack_count,0)
+rack_count = place_rack_pair([0,4*2.7,0],rack_count,0)
